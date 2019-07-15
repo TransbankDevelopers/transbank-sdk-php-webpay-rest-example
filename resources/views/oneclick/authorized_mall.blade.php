1a0131bf5470bc3f944957c91ed2894c5edacdd6
Eugenio Nicolás López Ramos <eugenionlopez@gmail.com>
2019-07-15 17:44:53 -0400

    Add more Oneclick mall transaction operations
    - Add Authorization
    - Add deletion
    - Add Refund
    - Add status

---
+++ resources/views/oneclick/authorized_mall.blade.php
@@ -0,0 +1,37 @@
+
+<h1>Request</h1>
+{{ var_dump($req) }}
+
+
+<h1>Response</h1>
+{{ var_dump($resp) }}
+
+
+<h2>Estado de la transacción</h2>
+<form method="post" action="/oneclick/mall/transactionStatus" >
+    @csrf
+    <label>Buy order: {{ $resp->getBuyOrder() }}</label>
+    <input type="hidden" name="buy_order" value="{{  $resp->getBuyOrder() }}">
+
+    <button type="submit">Enviar datos</button>
+</form>
+
+
+
+<h2>Reembolso de la transacción</h2>
+<form method="post" action="/oneclick/mall/refund">
+    @csrf
+    <label>Buy order padre</label>
+    <input name="parent_buy_order" value="{{ $resp->getBuyOrder() }}">
+
+    <label>Commerce code hijo</label>
+    <input name="commerce_code" value="{{ $resp->getDetails()[0]["commerce_code"] }}">
+
+    <label>Buy order hijo</label>
+    <input name="child_buy_order" value="{{ $resp->getDetails()[0]["buy_order"] }}">
+
+    <label>Monto</label>
+    <input name="amount" value="{{ $resp->getDetails()[0]["amount"] }}"/>
+
+    <button type="submit">Enviar</button>
+</form>
---
+++ resources/views/oneclick/mall_refund_transaction.blade.php
@@ -0,0 +1,7 @@
+<h1>Oneclick mall refund exitoso</h1>
+
+<h1>Request</h1>
+{{ var_dump($req) }}
+
+<h1>Response</h1>
+{{ var_dump($resp) }}
