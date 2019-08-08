77200760b044f31eeb16dc3e6c20660d683e31d8
Nicolas Martinez <nmartinezv@icloud.com>
2019-08-07 23:57:18 -0400

    add patpass comercio

---
+++ resources/views/patpass_comercio/inscription_start.blade.php
@@ -0,0 +1,6 @@
+<h1>Patpass Comercio Inscription</h1>
+
+<form action="post" action="patpassInscriptionStart" style="display: flex; flex-direction:column; width:50%;font-size: 20px;">
+    @csrf
+    <label for="userName"></label>
+</form>
