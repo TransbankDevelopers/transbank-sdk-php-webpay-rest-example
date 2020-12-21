e8f9429428c311486a770030cacda90f46e8b8a8
Felipe Fiebig <felipe.fiebig@continuum.cl>
2020-12-21 16:00:02 -0300

    Add Webpay Plus Mall Deferred

--- resources/views/webpayplus/mall/diferido/create.blade.php
+++ resources/views/webpayplus/mall/diferido/create.blade.php
@@ -12,12 +12,8 @@
     <style>
         .webpay_form input {
             font-size: 20px;
-
         }
-
-
     </style>
-
 </head>
 
 <body>
@@ -30,24 +26,30 @@
     <input id="merchant_1_amount" name="detail[0][amount]" value="1000">
 
     <label for="merchant_1_commerce_code">Codigo comercio del comercio 1</label>
-    <input id="merchant_1_commerce_code" name="detail[0][commerce_code]" value="597055555545">
+    @if (app()->environment('production'))
+        <?php $childCC = config('services.transbank.webpay_plus_mall_deferred_child_cc') ?>
+        <input id="merchant_1_commerce_code" name="detail[0][commerce_code]" value="{{ $childCC }}">
+    @else
+        <input id="merchant_1_commerce_code" name="detail[0][commerce_code]" value="597055555545">
+    @endif
 
     <label for="merchant_1_buy_order">Orden de compra comercio 1</label>
     <input id="merchant_1_buy_order" name="detail[0][buy_order]" value="{{ time() + rand(1,1000) }}">
 
+    @if (!app()->environment('production'))
+        <label for="merchant_2_amount">Monto comercio 2</label>
+        <input id="merchant_2_amount" name="detail[1][amount]" value="2000">
 
-    <label for="merchant_2_amount">Monto comercio 2</label>
-    <input id="merchant_2_amount" name="detail[1][amount]" value="2000">
+        <label for="merchant_2_commerce_code">Codigo comercio del comercio 2</label>
+            <input id="merchant_1_commerce_code" name="detail[1][commerce_code]" value="597055555546">
 
-    <label for="merchant_2_commerce_code">Codigo comercio del comercio 2</label>
-    <input id="merchant_2_commerce_code" name="detail[1][commerce_code]" value="597055555546">
-
-    <label for="merchant_2_buy_order">Orden de compra comercio 2</label>
-    <input id="merchant_2_buy_order" name="detail[1][buy_order]" value="{{ time() + rand(1, 1000) }}">
+        <label for="merchant_2_buy_order">Orden de compra comercio 2</label>
+        <input id="merchant_2_buy_order" name="detail[1][buy_order]" value="{{ time() + rand(1, 1000) }}">
 
 
-    <label for="parent_merchant_buy_order">Orden de compra comercio Padre</label>
-    <input id="parent_merchant_buy_order" name="buy_order" value="{{ time() + rand(1,1000) }}"/>
+        <label for="parent_merchant_buy_order">Orden de compra comercio Padre</label>
+        <input id="parent_merchant_buy_order" name="buy_order" value="{{ time() + rand(1,1000) }}"/>
+    @endif
 
     <label for="session_id_parent">
         Session id comercio padre
