188f09d397a7558b6151ced615832a7891190e8e
Matias Vasquez <m.vasquezac@gmail.com>
2024-02-14 16:52:17 -0300

    feat: add new commerce created view

---
+++ resources/views/webpayplus/mall_dues_qr/mall_transaction_created.blade.php
@@ -0,0 +1,21 @@
+<h1> Ejemplo Webpay Plus Transacción Mall</h1>
+
+<h3>Parametros recibidos:</h3>
+<pre>
+    {{ print_r($params) }}
+</pre>
+
+
+<h3>Respuesta:</h3>
+<pre>
+    {{ print_r($response)  }}
+</pre>
+
+<form method="post" action={{  $response->getUrl() }}>
+    <input name="token_ws" value={{ $response->getToken() }} />
+
+    <button type="submit">Enviar datos</button>
+</form>
+
+
+
