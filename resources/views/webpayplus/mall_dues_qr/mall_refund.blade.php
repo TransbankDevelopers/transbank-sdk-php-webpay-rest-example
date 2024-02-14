5e7f0cf844636439797fd356c43d206c6ed8bb34
Matias Vasquez <m.vasquezac@gmail.com>
2024-02-14 16:51:02 -0300

    feat: add new commerce refund

---
+++ resources/views/webpayplus/mall_dues_qr/mall_refund.blade.php
@@ -0,0 +1,18 @@
+@extends('layout')
+@section('content')
+    <h1> Ejemplo Webpay Plus Mall Refund </h1>
+
+
+    <form method="post" action="refund">
+        @csrf
+        <label for="amount">Monto de la reversa</label>
+        <input id="amount" name="amount" value="1000">
+
+        <label for="token">Token</label>
+        <input id="token" name="token" value="e229435b68a3f1c048121d68cc4ccb0b8765abd82d61d01e6cd2334db1473375">
+
+        <button type="submit">Enviar datos</button>
+
+
+    </form>
+@endsection
