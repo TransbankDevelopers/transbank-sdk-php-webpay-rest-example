5dc633d3cb78e7bd6c4353e8e2fb0ab60c4b660e
Mauricio Astudillo Toledo <m.astudillo.toledo@gmail.com>
2025-12-01 19:02:06 +0000

    feat: add start inscription view

---
+++ resources/views/oneclick/standard_brand/start_inscription.blade.php
@@ -0,0 +1,19 @@
+@extends('layout')
+@section('content')
+        <h1>Oneclick inscripción</h1>
+
+        <form method="post" action="startInscription"  style="display: flex; flex-direction:column; width:50%;font-size: 20px;">
+            @csrf
+            <label for="userName">Nombre de usuario</label>
+            <input id="userName" name="user_name" value="nombre_de_usuario">
+
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" value="example@example.com">
+
+            <label for="responseUrl">URL de Respuesta</label>
+            <input id="responseUrl" name="response_url" value="{{ url('/') }}/oneclick/standard_brand/responseUrl">
+
+
+            <button type="submit">Enviar datos</button>
+        </form>
+@endsection
