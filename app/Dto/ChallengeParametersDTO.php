80161d765613bc3485bb637761115f1ad3e75983
Matias Vasquez <m.vasquezac@gmail.com>
2025-12-01 22:33:48 -0300

    feat: add challenge data dto

---
+++ app/Dto/ChallengeParametersDTO.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Dto;
+
+class ChallengeParametersDTO
+{
+    /** @var string */
+    private $browserChallengeToken;
+
+    public function __construct(string $browserChallengeToken)
+    {
+        $this->browserChallengeToken = $browserChallengeToken;
+    }
+
+    public static function fromArray(array $data): self
+    {
+        return new self($data['browserChallengeToken']);
+    }
+
+    public function getBrowserChallengeToken(): string
+    {
+        return $this->browserChallengeToken;
+    }
+}
