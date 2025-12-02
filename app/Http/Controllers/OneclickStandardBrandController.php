d440d2574a1120b848d678851fb22f740a9fa692
Matias Vasquez <m.vasquezac@gmail.com>
2025-12-01 22:40:07 -0300

    feat: add missing methods on controller

--- app/Http/Controllers/OneclickStandardBrandController.php
+++ app/Http/Controllers/OneclickStandardBrandController.php
@@ -65,4 +65,73 @@ class OneclickStandardBrandController extends Controller
         $resp = $this->standardBrandService->deleteInscription($tbkUser, $userName);
         return view('oneclick/standard_brand/mall_inscription_deleted', ["req" => $req, "resp" => $resp]);
     }
+
+    //TODO: Confirm that this method is Ok
+    public function transactionStatus(Request $request)
+    {
+        $req = $request->except('_token');
+        $buyOrder = $req["buy_order"];
+
+        $resp = $this->standardBrandService->status($buyOrder);
+
+        return view('oneclick/standard_brand/mall_transaction_status', ["req" => $req, "resp" => $resp]);
+    }
+
+    //TODO: Confirm that this method is Ok
+    public function refund(Request $request)
+    {
+        $req = $request->except('_token');
+        $buyOrder = $req["parent_buy_order"];
+        $childCommerceCode = $req["commerce_code"];
+        $childBuyOrder = $req["child_buy_order"];
+        $amount = $req["amount"];
+
+        $resp = $this->standardBrandService->refund($buyOrder, $childCommerceCode, $childBuyOrder, $amount);
+
+        return view('oneclick/standard_brand/mall_refund_transaction', ["req" => $req, "resp" => $resp]);
+    }
+
+    //TODO: Confirm that this method is Ok
+    public function authorizeBrandStandard(Request $request)
+    {
+        //TODO: Get these values from the request
+        $details = [
+            'amount' => 50,
+            'buy_order' => 'order12345',
+            'commerce_code' => config('services.oneclick_mall_standard_brand_child_cc') ?? "",
+            'pmnt_ind' => 'C',
+            'recur_pmnt' => 'V',
+            'tid' => '',
+            'device_type' => '',
+            'browserAcceptHeader' => '',
+            'browserIP' => '',
+            'browserJavaEnabled' => '',
+            'browserLanguage' => '',
+            'browserScreenHeight' => '',
+            'browserScreenWidth' => '',
+            'browserTZ' => '',
+            'browserUserAgent' => '',
+            'browserJavascriptEnabled' => true,
+            'installments_number' => 0,
+        ];
+
+        try {
+            //TODO: Get these values from the request
+            $response = $this->standardBrandService->authorize('user-name', 'tbk_user_123', 'order12345', 1, 'NO', $details);
+            if($response instanceof \App\Dto\ChallengeResponseDTO){
+                //TODO: Handle brand challenge response
+
+            }
+            else if($response instanceof \App\Dto\AuthorizeResponseDTO){
+                //TODO: Handle no challenge response
+
+            }
+            return response()->json($response);
+        }
+
+        catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+
+    }
 }
