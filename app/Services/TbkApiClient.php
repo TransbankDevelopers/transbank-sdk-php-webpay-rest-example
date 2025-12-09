ebe24fd32aa1ea8cdc783023dfd9e6e0d5f0644b
Matias Vasquez <m.vasquezac@gmail.com>
2025-12-09 11:30:39 -0300

    feat: delete mock response

--- app/Services/TbkApiClient.php
+++ app/Services/TbkApiClient.php
@@ -7,57 +7,6 @@ class TbkApiClient
     protected string $apiKeyId;
     protected string $apiKeySecret;
 
-    protected array $mockResponses = [
-        'no_challenge_response' => [
-                'status' => 200,
-                'response' => [
-                    'buy_order' => 'OrdenCompra',
-                    'card_detail' => [
-                        'card_number' => '123456****7890',
-                    ],
-                    'accounting_date' => '2024-10-01',
-                    'transaction_date' => '2024-10-01T12:00:00Z',
-                    'request_3ds_authentication' => 'YES',
-                    'Details' => [
-                        'amount' => 50,
-                        'status' => 'AUTHORIZED',
-                        'tid' => '123456789',
-                        'authorization_code' => 'AuthCode123',
-                        'payment_type_code' => 'VN',
-                        'response_code' => 0,
-                        'installments_number' => 0,
-                        'commerce_code' => '597020000541',
-                        'buy_order' => 'order12345',
-                        'recur_pmnt' => 'V',
-                        'eci_trx_res' => '7',
-                        'pmnt_ind' => 'C',
-                    ]
-                ]
-            ],
-        'challenge_response' => [
-                'status' => 200,
-                'response' => [
-                    'challenge_data' => [
-                        'base_url' => 'https://mock-url.test',
-                        'timeout' => '300',
-                        'redirect_method' => 'POST',
-                        'target_type' => 'REDIRECT_NO_BANK_IFRAME',
-                        'parameters' => [
-                            'browserChallengeToken' => 'MockToken123'
-                        ]
-                    ]
-                ]
-            ],
-        'start_response' => [
-                'status' => 200,
-                'response' => [
-                    'token' => 'MockToken1234567890',
-                    'url_webpay' => 'https://mock-inscription-url.test/inscription'
-                ]
-            ]
-
-    ];
-
     public function __construct(string $apiKeyId, string $apiKeySecret)
     {
         $this->apiKeyId     = $apiKeyId;
