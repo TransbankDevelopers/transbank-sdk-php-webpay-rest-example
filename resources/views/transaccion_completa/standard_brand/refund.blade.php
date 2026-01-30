db76de375982f5c821b838f01f1bc05ca4e46001
victor mendoza <victor.mendoza@continuumhq.com>
2026-01-30 18:46:13 +0000

    feat: add transaction complete standar brand refund view

---
+++ resources/views/transaccion_completa/standard_brand/refund.blade.php
@@ -0,0 +1,23 @@
+@extends('layout')
+@section('content')
+    <h1>Reembolso - Transacción Completa Standard Brand</h1>
+
+    <h3>Parámetros recibidos:</h3>
+    <pre>
+    {{ print_r($req) }}
+</pre>
+
+    <h3>Respuesta:</h3>
+    <pre>
+    {{ print_r($res) }}
+</pre>
+
+
+    <h3>Consultar estado</h3>
+    <form class="webpay_form sb-form" action="/transaccion_completa/standard_brand/status" method="post">
+        @csrf
+        <label for="token">Token</label>
+        <input type="text" name="token" value="{{ $req['token'] }}">
+        <button type="submit">Consultar estado</button>
+    </form>
+@endsection
