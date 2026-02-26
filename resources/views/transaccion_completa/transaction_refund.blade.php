3106b3a154c2f95a33411d8d70ad293cadb8f38f
Mauricio Astudillo Toledo <m.astudillo.toledo@gmail.com>
2026-02-26 14:49:14 +0000

    refactor: normalize name for refund view

--- resources/views/transaccion_completa/transaction_refund.blade.php
+++ resources/views/transaccion_completa/transaction_refund.blade.php
@@ -1,1 +1,18 @@
-<?php
+@extends('layout')
+@section('content')
+<h1> Ejemplo Transacción Completa Refund</h1>
+
+<h3>Parametros recibidos:</h3>
+<pre>
+    {{ print_r($req) }}
+</pre>
+
+
+<h3>Respuesta:</h3>
+<pre>
+    {{ print_r($res)  }}
+</pre>
+
+<br>
+<a href="/transaccion_completa/create"><h1>Volver</h1></a>
+@endsection
