db2cc51abbf26ef6daa1a0a06676a05737695f79
Matias Vasquez <m.vasquezac@gmail.com>
2026-03-11 10:24:28 -0300

    feat: add option to authorize directly

--- resources/views/oneclick/standard_brand/start_inscription.blade.php
+++ resources/views/oneclick/standard_brand/start_inscription.blade.php
@@ -2,7 +2,7 @@
 @section('content')
         <h1>Oneclick inscripción</h1>
 
-        <form method="post" action="startInscription"  style="display: flex; flex-direction:column; width:50%;font-size: 20px;">
+        <form method="post" action="startInscription"  style="display: flex; flex-direction:column; width:80%;font-size: 20px;">
             @csrf
             <label for="userName">Nombre de usuario</label>
             <input id="userName" name="user_name" value="nombre_de_usuario">
@@ -15,5 +15,11 @@
 
 
             <button type="submit">Enviar datos</button>
+        </form>
+        <hr style="margin: 10px;">
+        <h1>Autorizar con cuenta existente</h1>
+        <p style="width: 80%;">Si ya tienes una cuenta de usuario previamente enrolada, puedes autorizar directamente.</p>
+        <form method="get" action="/oneclick/standard_brand/mall/direct-authorize" style="display: flex; flex-direction:column; width:80%;font-size: 20px;">
+            <button type="submit">Ir a autorizar</button>
         </form>
 @endsection
