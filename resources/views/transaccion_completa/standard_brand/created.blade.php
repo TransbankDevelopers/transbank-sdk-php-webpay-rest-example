9d537ca33b99514a0b700a1eb87d1f29ae417cef
Matias Vasquez <m.vasquezac@gmail.com>
2026-05-15 16:24:36 -0400

    feat: add commit form on created view

--- resources/views/transaccion_completa/standard_brand/created.blade.php
+++ resources/views/transaccion_completa/standard_brand/created.blade.php
@@ -30,4 +30,20 @@
 
         <button type="submit">Consultar cuotas</button>
     </form>
+
+    <form class="webpay_form sb-form" action="/transaccion_completa/standard_brand/commit" method="post">
+        @csrf
+        <label for="token">Token</label>
+        <input type="text" name="token" value="{{ $res['token'] ?? '' }}">
+
+        <label for="commerce_code">Código de comercio</label>
+        <input type="text" name="commerce_code" value="{{ $req['details'][0]['commerce_code'] ?? '' }}">
+
+        <label for="buy_order">Orden de compra (hijo)</label>
+        <input type="text" name="buy_order" value="{{ $req['details'][0]['buy_order'] ?? '' }}">
+
+        <input type="hidden" name="amount" value="{{ $req['details'][0]['amount'] ?? '' }}" />
+
+        <button type="submit">Confirmar transacción</button>
+    </form>
 @endsection
